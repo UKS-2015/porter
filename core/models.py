fea1d9937daa7344dd3d88d63a4edd60b9548fe6
novak <gnovak.boskov@gmail.com>
2016-03-12 10:50:13 +0100

    Initial model
    - This is approximately what we modeled few days ago

--- core/models.py
+++ core/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+
+class Project(models.Model):
+    title = models.CharField(max_length=50)
+    users = models.ManyToManyField(User)
+
+class Repository(models.Model):
+    title = models.CharField(max_length=50)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+class Milestone(models.Model):
+    title = models.CharField(max_length=50)
+
+class Label(models.Model):
+    title = models.CharField(max_length=50)
+
+class Issue(models.Model):
+    title = models.CharField(max_length=50)
+    creator = models.ForeignKey(User, related_name='issues')
+    assignee = models.ForeignKey(User)
+    repository  = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    milestone = models.ForeignKey(Milestone)
+    labels = models.ManyToManyField(Label)
+
+class UserProjectRole(models.Model):
+    role = models.CharField(max_length=30)
+    user = models.ManyToManyField(User)
+    repository = models.ManyToManyField(Repository)
+
+class IssueLog(models.Model):
+    content = models.TextField()
+    log_type = models.IntegerField()
+    object_user = models.ForeignKey(User, related_name='issue_log_object')
+    subject_user = models.ForeignKey(User, related_name='issue_log_subject')
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
