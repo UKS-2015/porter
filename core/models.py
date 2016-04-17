65d7940b693173f18ab1d6dbe8b61400807d43e7
Lazar Nikolić <lazarpwna@gmail.com>
2016-04-17 17:34:23 +0200

    Project templates now use django-bootstrap3.

--- core/models.py
+++ core/models.py
@@ -4,18 +4,18 @@ from django.contrib.auth.models import Group, User
 from django.db import models
 from django.core.urlresolvers import reverse
 
-# class User(models.Model):
-#     first_name = models.CharField(max_length=50)
-#     last_name = models.CharField(max_length=50)
-#     username = models.CharField(max_length=50)
-#     class Meta:
-#         permissions = [
-#             ('read_user', 'Can access detailed view for users.'),
-#             ('view_user', 'Can access the user application.')
-#         ]
-#
-#     def __str__(self):
-#         return self.username
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    class Meta:
+        permissions = [
+            ('read_user', 'Can access detailed view for users.'),
+            ('view_user', 'Can access the user application.')
+        ]
+
+    def __str__(self):
+        return self.username
 
     def to_dict(instance):
         opts = instance._meta
