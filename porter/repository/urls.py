f6cedc0e4fbba47c29a7906840de46e45cdbf276
Lazar Nikolić <lazarpwna@gmail.com>
2016-04-21 11:18:14 +0200

    Small fixes to templates and urls.

--- porter/repository/urls.py
+++ porter/repository/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from porter.repository import views
 
+app_name = "repository"
+
 urlpatterns = [
-    url(r'^$', views.list_all, name='repo_list_all'),
-    url(r'^new/$', views.create, name='repo_create'),
+    url(r'^$', views.list_all, name='list_all'),
+    url(r'^new/$', views.create, name='create'),
     # TODO: Changing repository name will break links - only allow deleting
-    url(r'(?P<repository_title>[^/\\]+)/change/$', views.change, name='repo_change'),
-    url(r'(?P<repository_title>[^/\\]+)/delete/$', views.delete, name='repo_delete'),
-    url(r'(?P<repository_title>[^/\\]+)/$', views.overview, name='repo_overview'),
+    url(r'(?P<repository_title>[^/\\]+)/change/$', views.change, name='change'),
+    url(r'(?P<repository_title>[^/\\]+)/delete/$', views.delete, name='delete'),
+    url(r'(?P<repository_title>[^/\\]+)/$', views.overview, name='overview'),
 ]
