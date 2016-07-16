6f93c51aa9f47fce3b36f452ef188bb3ab8f436f
Lazar Nikolić <lazarpwna@gmail.com>
2016-07-16 13:01:27 +0200

    Closes #42

---
+++ porter/repository/tests/test_form.py
@@ -0,0 +1,39 @@
+from core.forms import RepositoryForm
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+
+class RepositoryTests(TestCase):
+    fixtures = ['test_fixture.json']
+
+    def test_form_valid(self):
+        form = RepositoryForm(data={'title': 'Repository', 'description': 'Repository description'})
+        self.assertTrue(form.is_valid(), 'Repository form should be valid: %s' % form.errors)
+
+    def test_form_invalid(self):
+        # Missing values
+        form = RepositoryForm(data={'title': 'Test'})
+        self.assertFalse(form.is_valid(), 'Repository form should be invalid: %s' % form.errors)
+
+        form = RepositoryForm(data={'description': 'Test description'})
+        self.assertFalse(form.is_valid(), 'Repository form should be invalid: %s' % form.errors)
+
+    def test_repository_update_form(self):
+        logged_in = self.client.login(username='owner', password='admin1234')
+        self.assertTrue(logged_in)
+
+        response = self.client.get(
+            reverse(
+                'project:repository:overview',
+                kwargs={'project_title': 'Test', 'repository_title': 'Test repository'}
+            )
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('repository' in response.context)
+
+        repository = response.context['repository']
+        # Form would be invalid because of uniqueness constraint
+        # We know that it exists because we fetched it, so we can change its title
+        repository.title = 'New title'
+        form = RepositoryForm(data={'title': repository.title, 'description': repository.description})
+        self.assertTrue(form.is_valid(), form.errors)
---
+++ porter/repository/tests/test_view.py
@@ -0,0 +1,95 @@
+from core.models import Project, UserProjectRole, Repository
+from django.contrib.auth.models import User, Group
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+GUEST_ROLE = 'Guest'
+OWNER_ROLE = 'Project owner'
+LEAD_ROLE = 'Project lead'
+DEVELOPER_ROLE = 'Developer'
+
+
+class RepositoryTests(TestCase):
+    fixtures = ['test_fixture.json']
+
+    def setUp(self):
+        self.repository = Repository.objects.get(title='Test repository')
+        self.project = Project.objects.get(title='Test')
+        logged_in = self.client.login(username='owner', password='admin1234')
+        self.assertTrue(logged_in)
+
+    def test_repository_list(self):
+        self.porter_project = Project.objects.get(title='Porter')
+        response = self.client.get(
+            reverse('project:repository:list_all', kwargs={'project_title': self.repository.project.title})
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.context['repository_list'], [self.repository.to_dict()])
+
+    def test_add_repository(self):
+        repository_title = 'New repository'
+        description = 'New repository description'
+        response = self.client.post(
+            reverse('project:repository:create', kwargs={'project_title': self.repository.project.title}),
+            {'title': repository_title, 'description': description}
+        )
+        self.assertRedirects(
+            response,
+            reverse('project:repository:list_all', kwargs={'project_title': self.repository.project.title})
+        )
+
+        new_repository_added = Repository.objects.filter(title=repository_title).exists()
+        self.assertTrue(new_repository_added)
+
+    def test_repository_overview(self):
+        response = self.client.get(
+            reverse(
+                'project:repository:overview',
+                kwargs={'project_title': self.repository.project.title, 'repository_title': self.repository.title}
+            )
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.context['repository'], self.repository)
+
+    def test_repository_update(self):
+        response = self.client.get(
+            reverse(
+                'project:repository:change',
+                kwargs={'project_title': self.repository.project.title, 'repository_title': self.repository.title}
+            )
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.context['repository'], self.repository)
+
+        new_title = 'New title'
+        new_description = 'New description'
+        response = self.client.post(
+            reverse(
+                'project:repository:change',
+                kwargs={'project_title': self.repository.project.title, 'repository_title': self.repository.title}
+            ),
+            {'title': new_title, 'description': new_description}
+        )
+        self.assertRedirects(
+            response,
+            reverse('project:repository:list_all', kwargs={'project_title': self.repository.project.title})
+        )
+
+        repository = Repository.objects.get(pk=self.repository.id)
+        self.assertEquals(repository.title, new_title)
+        self.assertEquals(repository.description, new_description)
+
+    def test_repository_delete(self):
+        response = self.client.post(
+            reverse(
+                'project:repository:delete',
+                kwargs={'project_title': self.repository.project.title, 'repository_title': self.repository.title}
+            )
+        )
+        self.assertRedirects(
+            response,
+            reverse('project:repository:list_all', kwargs={'project_title': self.repository.project.title})
+        )
+
+        repository_exists = Repository.objects.filter(pk=self.repository.id).exists()
+        self.assertFalse(repository_exists)
