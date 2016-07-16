b9827707f0966cd1560c0e5e39aa40f8e557dee4
BojanaZoranovic <bojana.zoranovic@hotmail.com>
2016-07-16 04:57:22 +0200

    Removing  Add issue option from project

--- porter/repository/urls.py
+++ porter/repository/urls.py
@@ -9,10 +9,10 @@ app_name = "repository"
 urlpatterns = [
     url(r'^$', RepositoryList.as_view(), name='list_all'),
     url(r'^new/$', RepositoryCreate.as_view(), name='create'),
+    url(r'(?P<repository_title>[^/\\]+)/issues/', include(issue_urls), name='issue'),
+    url(r'(?P<repository_title>[^/\\]+)/milestones/', include(milestone_urls), name='milestone'),
     # TODO: Changing repository name will break links - only allow deleting
     url(r'(?P<repository_title>[^/\\]+)/change/$', RepositoryUpdate.as_view(), name='change'),
     url(r'(?P<repository_title>[^/\\]+)/delete/$', RepositoryDelete.as_view(), name='delete'),
-    url(r'(?P<repository_title>[^/\\]+)/issues/', include(issue_urls), name='issues'),
-    url(r'(?P<repository_title>[^/\\]+)/milestones/', include(milestone_urls), name='milestone'),
     url(r'(?P<repository_title>[^/\\]+)/$', RepositoryOverview.as_view(), name='overview'),
 ]
