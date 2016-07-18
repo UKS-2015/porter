0263b16fa8b3610c6072e2a27b9b82d8635af527
BojanaZoranovic <bojana.zoranovic@hotmail.com>
2016-07-18 09:27:11 +0200

    Tests for Issue, Label, Milestone.

---
+++ porter/label/tests/test_view.py
@@ -0,0 +1,79 @@
+from core.models import Label, UserProjectRole, Repository, Issue
+from django.contrib.auth.models import User, Group
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+class IssueTests(TestCase):
+    fixtures = ['test_fixture.json']
+
+    def setUp(self):
+        self.labels = Label.objects.filter(project = 4)
+        self.label = Label.objects.get(pk=2)
+        logged_in = self.client.login(username='owner', password='admin1234')
+        self.assertTrue(logged_in)
+
+    def test_label_list(self):
+        response = self.client.get(
+            reverse('project:label:list', kwargs={'project_title': self.label.project.title})
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(len([x for x in response.context['label_list'] if x in self.labels]), len(self.labels))
+
+    # def test_add_label(self):
+    #     title = 'New label'
+    #     color = '#33ff77'
+    #     response = self.client.post(
+    #         reverse('project:label:new', kwargs={'project_title': self.label.project.title}),
+    #         {'title': title, 'color': color}
+    #     )
+    #     self.assertRedirects(
+    #         response,
+    #         reverse('project:label:list', kwargs={'project_title': self.label.project.title})
+    #     )
+    #
+    #     new_label_added = Label.objects.filter(title=title).exists()
+    #     self.assertTrue(new_label_added)
+    #     self.assertTrue(Label.objects.filter(title=title)[0].color,color)
+    #     self.assertTrue(Label.objects.filter(title=title)[0].project,self.label.project)
+    # #
+    # def test_issue_overview(self):
+    #     response = self.client.get(
+    #         reverse(
+    #             'project:label:overview',
+    #             kwargs={'project_title': self.label.project.title, 'pk':self.label.pk}
+    #         )
+    #     )
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertEquals(response.context['project_title'], self.label.project.title)
+
+    def test_issue_update(self):
+        response = self.client.get(
+            reverse(
+                'project:label:change',
+                kwargs={'project_title': self.label.project.title,'pk':self.label.pk}
+            )
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.context['form'].instance, self.label)
+
+        new_title = 'New title'
+        new_color = '#33ff77'
+        response = self.client.post(
+            reverse(
+                'project:label:change',
+                kwargs={'project_title': self.label.project.title, 'pk':self.label.pk}
+            ),
+            {'title': new_title, 'color': new_color}
+        )
+        self.assertRedirects(
+            response,
+            reverse(
+                'project:label:list',
+                kwargs={'project_title': self.label.project.title}
+            )
+        )
+
+        newLabel = Label.objects.get(pk=self.label.id)
+        self.assertEquals(newLabel.title, new_title)
+        self.assertEquals(newLabel.color, new_color)
+        self.assertEquals(newLabel.project, self.label.project)
---
+++ porter/milestone/tests/test_form.py
@@ -0,0 +1,38 @@
+from core.forms import MilestoneForm, MilestoneWithRepoForm
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from core.models import Milestone
+
+
+class MilestoneFormTests(TestCase):
+    fixtures = ['test_fixture.json']
+
+    def test_form_valid(self):
+        user = User.objects.get(username='owner')
+        form = MilestoneForm(data={'title': 'Milestone', 'repository': 5, 'description': 'Milestone description'})
+        self.assertTrue(form.is_valid(), 'MilestoneForm should be valid: %s' % form.errors)
+
+    def test_form_invalid(self):
+        # Missing values
+        form = MilestoneForm(data={'title': 'Test'})
+        self.assertFalse(form.is_valid(), 'MilestoneForm should be invalid: %s' % form.errors)
+
+        form = MilestoneForm(data={'description': 'Test description'})
+        self.assertFalse(form.is_valid(), 'Project form should be invalid: %s' % form.errors)
+
+    def test_milestone_update_form(self):
+        logged_in = self.client.login(username='owner', password='admin1234')
+        self.assertTrue(logged_in)
+
+        response = self.client.get(
+            reverse(
+                'project:repository:milestone:change',
+                kwargs={'project_title': 'Test', 'repository_title': 'Test repository', 'pk':1}
+            )
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.context['repository_title'], 'Test repository')
+        self.assertEquals(response.context['project_title'], 'Test')
+        #self.assertEquals(response.context['pk'], 1)
+
